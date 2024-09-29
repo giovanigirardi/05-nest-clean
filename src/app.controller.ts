54433a8ddc694fd7e244172246d0290e5e031770
Giovani Girardi <giovanigg08@gmail.com>
2024-09-29 19:25:05 -0300

    feat: :sparkles: inject prisma client into app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
 import type { AppService } from "./app.service";
+import type { PrismaService } from "./prisma/prisma.service";
 
 @Controller("/api")
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(
+		private readonly appService: AppService,
+		private readonly prisma: PrismaService,
+	) {}
 
 	@Get("/hello")
 	getHello(): string {
